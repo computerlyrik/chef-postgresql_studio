6c17e572f90e962fb0dc1bf9e64301c94c0aad84
Christian Fischer <sw-dev@computerlyrik.de>
2015-01-17 06:09:24 +0100

    berks init

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'postgresql_studio'
+maintainer       'computerlyrik, Christian Fischer'
+maintainer_email 'chef-cookbooks@computerlyrik.de'
+license          'Apache 2.0'
+description      'Installs/Configures postgresql_studio'
+long_description 'Installs/Configures postgresql_studio'
+version          '0.1.0'
